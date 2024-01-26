9cc4bd1d2f5b6cb1541ca8a62f460f893b9ce0d3
Borja <brb0004@alu.medac.es>
2024-01-26 09:59:11 +0100

    Último comentario Main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Menu import Menu
-
+# Instanciamos dede nuestro main un objeto de la clase Menu para poder llamar al método (iniciar_juego)
 if __name__ == "__main__":
-    menu_juego = Menu()
-    menu_juego.iniciar_juego()
+    nuevoJuego = Menu()
+    nuevoJuego.iniciar_juego()
